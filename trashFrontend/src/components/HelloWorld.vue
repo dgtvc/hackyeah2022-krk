f59566a041d02f6c215f8e082d4d9762eb6ffa4d
Grzegorz Iżyk <grzegorz.izyk@outlook.com>
2022-11-19 11:44:14 +0100

    Setup vuetify

--- trashFrontend/src/components/HelloWorld.vue
+++ trashFrontend/src/components/HelloWorld.vue
@@ -5,14 +5,9 @@ defineProps<{
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
+  <v-btn>
+    HAckYeah
+  </v-btn>
 </template>
 
 <style scoped>
