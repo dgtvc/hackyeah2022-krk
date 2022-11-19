b2b9935825e177754d9ac3455da049ba4504f506
Mateusz Zahel <mateusz.zahel@gmail.com>
2022-11-19 13:14:08 +0100

    feat: Create base app layout

--- trashFrontend/src/App.vue
+++ trashFrontend/src/App.vue
@@ -3,12 +3,13 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <v-layout>
+    <v-app-bar :elevation="1">
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'add-new-location' }">Add new</RouterLink>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-layout>
 </template>
