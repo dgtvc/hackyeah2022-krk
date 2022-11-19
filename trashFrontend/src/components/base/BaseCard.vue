aa7a6bddcf9d381b815d6293e426348ec5bcbb93
Grzegorz Iżyk <grzegorz.izyk@outlook.com>
2022-11-19 22:07:10 +0100

    Refactor cards

---
+++ trashFrontend/src/components/base/BaseCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Props {
+  imgSrc: string;
+  title: string;
+  description: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <v-card color="#d3d3d3" class="mb-3">
+    <v-img height="150" :src="props.imgSrc" cover></v-img>
+
+    <v-card-title>{{ props.title }}</v-card-title>
+
+    <v-card-text>
+      {{ props.description }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="lighten-2" text> More details </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
