ca19de1f99922f66733eabda6acdcea35a395971
Grzegorz Iżyk <grzegorz.izyk@outlook.com>
2022-11-19 14:19:28 +0100

    Setup icons, add chips

--- trashFrontend/src/views/HomeView.vue
+++ trashFrontend/src/views/HomeView.vue
@@ -1,5 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const amenities = ref([1, 4]);
+</script>
 
 <template>
-  <main></main>
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <section class="leftBar">
+          <v-card-text>
+            <h2 class="text-h6 mb-2">Choose amenities</h2>
+
+            <v-chip-group v-model="amenities" column multiple>
+              <v-chip filter outlined> Elevator </v-chip>
+              <v-chip filter outlined> Washer / Dryer </v-chip>
+              <v-chip filter outlined> Fireplace </v-chip>
+              <v-chip filter outlined> Wheelchair access </v-chip>
+              <v-chip filter outlined> Dogs ok </v-chip>
+              <v-chip filter outlined> Cats ok </v-chip>
+            </v-chip-group>
+          </v-card-text>
+        </section>
+      </v-col>
+      <v-col cols="9">
+        <RouterView />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<style>
+.v-selection-control {
+  flex-direction: row-reverse;
+}
+</style>
