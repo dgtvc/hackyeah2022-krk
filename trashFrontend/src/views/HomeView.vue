c1236c906123e565f409f5df118339d3e6efb229
Grzegorz Iżyk <grzegorz.izyk@outlook.com>
2022-11-19 14:32:06 +0100

    Trash types and recycle types

--- trashFrontend/src/views/HomeView.vue
+++ trashFrontend/src/views/HomeView.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const amenities = ref([1, 4]);
+const selectedTrashTypes = ref([1, 4]);
+const selectedRecycleType = ref(1);
 </script>
 
 <template>
   <v-container>
     <v-row>
-      <v-col cols="3">
+      <v-col cols="4">
         <section class="leftBar">
           <v-card-text>
-            <h2 class="text-h6 mb-2">Choose amenities</h2>
+            <h2 class="text-h6 mb-2">Trash types</h2>
 
-            <v-chip-group v-model="amenities" column multiple>
-              <v-chip filter outlined> Elevator </v-chip>
-              <v-chip filter outlined> Washer / Dryer </v-chip>
-              <v-chip filter outlined> Fireplace </v-chip>
-              <v-chip filter outlined> Wheelchair access </v-chip>
-              <v-chip filter outlined> Dogs ok </v-chip>
-              <v-chip filter outlined> Cats ok </v-chip>
+            <v-chip-group v-model="selectedTrashTypes" column multiple>
+              <v-chip filter outlined> Glass </v-chip>
+              <v-chip filter outlined> Battery </v-chip>
+              <v-chip filter outlined> Electronic </v-chip>
+              <v-chip filter outlined> Medicines </v-chip>
+            </v-chip-group>
+          </v-card-text>
+          <v-card-text>
+            <h2 class="text-h6 mb-2">Recycle type</h2>
+
+            <v-chip-group v-model="selectedRecycleType" column>
+              <v-chip filter outlined> Glass </v-chip>
+              <v-chip filter outlined> Battery </v-chip>
+              <v-chip filter outlined> Electronic </v-chip>
             </v-chip-group>
           </v-card-text>
         </section>
       </v-col>
-      <v-col cols="9">
+      <v-col cols="8">
         <RouterView />
       </v-col>
     </v-row>
