4bd7ff76599684c85af6ba696ea027103abc152b
Grzegorz Iżyk <grzegorz.izyk@outlook.com>
2022-11-19 21:51:36 +0100

    Right bar

--- trashFrontend/src/views/HomeView.vue
+++ trashFrontend/src/views/HomeView.vue
@@ -26,9 +26,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-container fluid>
+  <v-container fluid class="pb-0">
     <v-row class="app-row">
-      <v-col cols="4">
+      <v-col cols="3">
         <section class="leftBar">
           <v-card-text>
             <h2 class="text-h6 mb-2">Location</h2>
@@ -71,7 +71,7 @@ onMounted(() => {
           </v-card-text>
         </section>
       </v-col>
-      <v-col cols="8" class="no-padding">
+      <v-col cols="6" class="no-padding">
         <BaseMap
           :map-config="{
             center: coordinates,
@@ -90,6 +90,50 @@ onMounted(() => {
           ]"
         />
       </v-col>
+      <v-col cols="3">
+        <section class="right-bar">
+          <v-card color="#d3d3d3" class="mb-3">
+            <v-img
+              height="150"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              cover
+            ></v-img>
+
+            <v-card-title>Cafe Badilico</v-card-title>
+
+            <v-card-text>
+              <div>
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="lighten-2" text> More details </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card color="#d3d3d3">
+            <v-img
+              height="150"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              cover
+            ></v-img>
+
+            <v-card-title>Cafe Badilico</v-card-title>
+
+            <v-card-text>
+              <div>
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="lighten-2" text> More details </v-btn>
+            </v-card-actions>
+          </v-card>
+        </section>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -100,5 +144,12 @@ onMounted(() => {
 }
 .app-row.v-row {
   margin: -16px;
+}
+.right-bar {
+  max-height: calc(100vh - 76px);
+  overflow-y: auto;
+}
+.v-col {
+  padding-bottom: 0 !important;
 }
 </style>
