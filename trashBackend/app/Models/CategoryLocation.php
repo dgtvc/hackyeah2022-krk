7cdf81cfb0c7a1671954d886ae25275699706681
Michal Szlagor <szlagormichal@gmail.com>
2022-11-20 10:47:45 +0100

    fix: remove primary key

--- trashBackend/app/Models/CategoryLocation.php
+++ trashBackend/app/Models/CategoryLocation.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryLocation extends Model
 {
     public $timestamps = false;
+    protected $primaryKey = null;
+    public $incrementing = false;
 
     protected $table = 'category_location';
 
