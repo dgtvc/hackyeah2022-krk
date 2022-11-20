62748f6c17a045b9bfee9096dbbb142ffb946643
Mateusz Zahel <mateusz.zahel@gmail.com>
2022-11-20 11:29:36 +0100

    feat: Change navigation btn colors

--- trashFrontend/src/components/app/AppNavigation.vue
+++ trashFrontend/src/components/app/AppNavigation.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-  <v-btn :to="{ name: 'home' }">Home</v-btn>
-  <v-btn :to="{ name: 'add-new-location' }">Add new</v-btn>
+  <v-btn color="primary" class="font-weight-medium" :to="{ name: 'home' }">
+    Home
+  </v-btn>
+  <v-btn
+    color="primary"
+    class="font-weight-bold"
+    :to="{ name: 'add-new-location' }"
+  >
+    Add new
+  </v-btn>
 </template>
